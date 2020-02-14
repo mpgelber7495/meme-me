8c082de1baaa98ab7c33e7916b6b415d494a8c95
Michael Gelber <mpgelber7495@gmail.com>
2020-02-14 16:34:20 -0500

    Feature: created a root index.js file for being able to run via node. Also downloaded some more NPM packages.

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+
+var app = express();
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(morgan("dev"));
+app.use(require("./routes"));
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`server running on ${process.env.PORT || PORT}`);
+});
