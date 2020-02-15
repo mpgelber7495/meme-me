df9c83705e7b1eeec2f2c2e5b41c5cc9b5e135bd
Michael Gelber <mpgelber7495@gmail.com>
2020-02-14 20:00:04 -0500

    Feature: Scraping sequelize CLI and doing the setup manually?

--- models/meme.js
+++ models/meme.js
@@ -1,23 +1,28 @@
 const sequelize = require("../config/config.json");
+const Sequelize = require("sequelize");
 const { DataTypes } = require("sequelize");
+const Model = Sequelize.Model;
 
-let Meme;
-
-module.exports = (sequelize, DataTypes) => {
-  Meme = sequelize.define("Meme", {
-    image_url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      isUrl: true
-    }
-  });
-
-  //   Meme.associate = function associate() {
-  //     Meme.belongsTo(User, {
-  //       foreignKey: {
-  //         allowNull: false
-  //       }
-  //     });
-  //   };
+module.exports = function(connection, Sequelize) {
+  var Meme = connection.define("meme", { image_url: DataTypes.STRING });
   return Meme;
 };
+
+// module.exports = (sequelize, DataTypes) => {
+//   Meme = sequelize.define("Meme", {
+//     image_url: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       isUrl: true
+//     }
+//   });
+
+//   Meme.associate = function associate() {
+//     Meme.belongsTo(User, {
+//       foreignKey: {
+//         allowNull: false
+//       }
+//     });
+//   };
+//   return Meme;
+// };
