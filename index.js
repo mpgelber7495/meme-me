d2cc8c990da867798fafd838f12c267d12a8a02f
Michael Gelber <mpgelber7495@gmail.com>
2020-02-18 20:29:00 -0500

    Feature: Started with the server side auth checking

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const { join } = require("path");
 
 //Auth0 integration
 const createAuth0Client = require("@auth0/auth0-spa-js");
+const jwt = require("express-jwt");
+const jwtAuthz = require("express-jwt-authz");
+const jwksRsa = require("jwks-rsa");
 
 var app = express();
 
@@ -19,8 +22,25 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(morgan("dev"));
 app.use(require("./routes"));
+
 //Auth0 integration
 app.use(express.static(join(__dirname, "public")));
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://YOUR_DOMAIN/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: "https://dev-ih2m-b95.auth0.com/api/v2/",
+  issuer: `https://YOUR_DOMAIN/`,
+  algorithms: ["RS256"]
+});
 
 app.get("/auth_config.json", (req, res) => {
   res.sendFile(join(__dirname, "auth_config.json"));
