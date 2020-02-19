483c12b0b6aaaca40eb947558ab06a771c77447b
Michael Gelber <mpgelber7495@gmail.com>
2020-02-18 21:31:02 -0500

    Feature: integrated all the previous functionality and deprecated all public folders as the index.js routes have taken their place

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const Meme = require("./models/meme");
 // const List = require("./models/list");
 
 var app = express();
@@ -15,6 +16,16 @@ app.use(express.urlencoded());
 app.use(morgan("dev"));
 app.use(require("./routes"));
 
+// route for displaying the homepage
+app.get("/", async (req, res) => {
+  let memes = await Meme.findAll({ raw: true });
+  res.render("home", { memes });
+});
+
+// route for displaying the add meme screen
+app.get("/add-meme", async (req, res) => {
+  res.render("addMeme");
+});
 app.listen(process.env.PORT || PORT, () => {
   console.log(`server running on ${process.env.PORT || PORT}`);
 });
