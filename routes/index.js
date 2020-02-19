366d3441b350395c55bc2fa5215ac45f64db53bd
Moon Jo <urwisdom77@gmail.com>
2020-02-18 20:13:21 -0500

    style: Add handlebars and setup public folderfor css and js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var router = require("express").Router();
+var Meme = require("../models/meme");
 
 router.use("/api", require("./api"));
-
+router.route("/").get(async (req, res) => {
+  const memes = await Meme.findAll();
+  res.render("home", { memes: memes });
+});
 module.exports = router;
