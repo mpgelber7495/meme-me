7dd9110b269738df6c10b425cfdd24a965697c44
Michael Gelber <mpgelber7495@gmail.com>
2020-02-24 14:27:12 -0500

    asdf

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.get("/", async (req, res) => {
   res.render("home", { memes, user: req.user });
 });
 models();
-// route for displaying the add meme screen
+// route for displaying the add meme screen a
 app.get("/add-meme", async (req, res) => {
   let userInfo = await req.user;
   console.log(userInfo);
