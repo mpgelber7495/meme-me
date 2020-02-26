6389dc6583f0f57dbaa3233ac4c5d91bed202a01
Michael Gelber <mpgelber7495@gmail.com>
2020-02-25 20:17:51 -0500

    Feature: Uploading now works with the req.user.id when posting to memes

--- routes/api/memes.js
+++ routes/api/memes.js
@@ -1,6 +1,7 @@
 var router = require("express").Router();
 var Meme = require("../../models/meme");
 var Comment = require("../../models/comment");
+var User = require("../../models/user");
 
 router
   .route("/")
@@ -11,7 +12,8 @@ router
   .post(async (req, res) => {
     console.log(req.user);
     if (req.user) {
-      req.body.UserId = req.user.id;
+      let userId = req.user.id.split("|")[1];
+      req.body.UserId = userId;
     }
     console.log(req.body);
 
