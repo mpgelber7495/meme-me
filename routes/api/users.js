f93d691115dbf1103aa3a04bee95ed7e4780ff9a
Michael Gelber <mpgelber7495@gmail.com>
2020-02-27 20:36:59 -0500

    Feature: Adding profile img to user table via post call server side

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,7 +7,8 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  console.log("DEBUG USER BODY", req.body);
+  // console.log("DEBUG req.user in post:", req.user);
+  req.body.profile_img = req.user.picture;
   const result = await User.create(req.body);
   res.json(result);
 });
