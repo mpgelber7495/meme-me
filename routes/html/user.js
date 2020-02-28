4953363d1cf5416393233a3ba52c40351c21aa3e
Moon Jo <urwisdom77@gmail.com>
2020-02-28 16:09:53 -0500

    chore: able to make user info show and then make horizontal scroll work

--- routes/html/user.js
+++ routes/html/user.js
@@ -12,38 +12,14 @@ router.get("/:id", async (req, res) => {
     where: { UserId: req.params.id },
     raw: true
   });
-  //   for (let i = 0; i < comments.length; i++) {
-  //     let likes = await Like.findAll({
-  //       where: { CommentId: comments[i].id },
-  //       raw: true
-  //     });
-  //     var likesLength = likes.filter(function(element) {
-  //       return element.up_or_down == 1;
-  //     });
-  //     var dislikesLength = likes.filter(function(element) {
-  //       return element.up_or_down == 0;
-  //     });
-
-  //     comments[i].likesArray = likes;
-  //     comments[i].likes = likesLength.length;
-  //     comments[i].dislikes = dislikesLength.length;
-  //     let user = await User.findAll({
-  //       where: { id: comments[i].UserId },
-  //       raw: true
-  //     });
-  //     comments[i].user = user[0].nickname;
-  //   }
-  //   let likes = await Like.findAll({
-  //     where: { MemeId: req.params.id },
-  //     raw: true
-  //   });
-  console.log("USER:: ", user);
+  console.log("USER:: ", user[0]);
   console.log("COMMENTS:: ", comments);
-  console.log("LIKES::", comments[0].likes);
+  console.log("LIKES::", comments[0]);
   //   console.log("LIKES:: ", likes);
   console.log(user[0]);
   res.render("userById", {
     memes: memes,
+    user: user[0],
     // creatingUser: user,
     comments: comments
   });
